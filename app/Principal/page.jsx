c6847f07ce333992cc46aaab5e1332f5ce13d78e
cristhiancastro204 <cristhiancastro204@gmail.com>
2026-03-02 21:51:26 -0300

    Soluciones2

---
+++ app/Principal/page.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+import {useState, useEffect} from "react";
+
+
+const principal = () => {
+  return (<h1>pagina principal</h1>);
+}
+export default principal;
--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,27 +1,26 @@
 "use client";
 
 import {useState} from "react";
-import { login } from "../api/api";
+import { register } from "../../api/api";
 import { useRouter } from "next/navigation";
 
 
-export default function Login() {
+export default function Home() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-
+  const [name, setName] = useState('');
   const router = useRouter();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const data = await login(username, password);
+    const data = await register(username, name, password);
     router.push("/Principal/");
   }
 
-    const handleSubmit2 = async (e) => {
+  const handleSubmit2 = async (e) => {
     e.preventDefault();
-    const data = await login(username, password);
-    router.push("/register/");
-    //ver si tenemos que redirigir a otra pagina o mostrar un mensaje de error
+    const data = await register(username, name, password);
+    router.push("/");
   }
 
 
@@ -30,7 +29,7 @@ export default function Login() {
     <div className="flex min-h-screen flex-col justify-center px-6 py-12 lg:px-8 bg-gray-900">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <h2 className="mt-10 text-center text-2xl font-bold text-white">
-          Sign In to your account
+          Create your account
         </h2>
       </div>
 
@@ -49,6 +48,23 @@ export default function Login() {
                 value = {username}
                 onChange={(e) => setUsername(e.target.value)}
                 autoComplete="given-name"
+                className="block w-full rounded-md bg-white/5 px-3 py-2 text-white outline outline-1 outline-white/10 focus:outline-2 focus:outline-indigo-500"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium text-gray-100">
+              Name
+            </label>
+            <div className="mt-2">
+              <input
+                id="name"
+                name="name"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                autoComplete="family-name"
                 className="block w-full rounded-md bg-white/5 px-3 py-2 text-white outline outline-1 outline-white/10 focus:outline-2 focus:outline-indigo-500"
               />
             </div>
@@ -77,15 +93,15 @@ export default function Login() {
               onClick={handleSubmit}
               className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-400"
             >
-              Sign In
+              Sign Up
             </button>
             <br></br>
             <button
-              type="button"
+              type="submit"
               onClick={handleSubmit2}
               className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-400"
             >
-              Sign Up
+              Sign In
             </button>
           </div>
 
